234c031fb973fed866d5a00476171b10fa39156c
mandresyRandria <zilberthomandresy@gmail.com>
2023-05-08 21:00:30 +0200

    gzip

--- mini_tp/app/Http/Kernel.php
+++ mini_tp/app/Http/Kernel.php
@@ -21,6 +21,8 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
+        \Symfony\Component\HttpFoundation\Response::class,
+        \App\Http\Middleware\GzipMiddleware::class,
     ];
 
     /**
