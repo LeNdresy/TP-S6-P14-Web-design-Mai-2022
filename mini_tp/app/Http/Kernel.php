f56aea9b7602c870e11f99c3432b4adc764f76b0
mandresyRandria <zilberthomandresy@gmail.com>
2023-05-08 21:40:36 +0200

    renew

--- mini_tp/app/Http/Kernel.php
+++ mini_tp/app/Http/Kernel.php
@@ -22,7 +22,7 @@ class Kernel extends HttpKernel
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
         //\Symfony\Component\HttpFoundation\Response::class,
-        \Illuminate\Foundation\Http\Middleware\CompressResponse::class,
+        //\Illuminate\Foundation\Http\Middleware\CompressResponse::class,
     ];
 
     /**
@@ -38,7 +38,7 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            \Illuminate\Foundation\Http\Middleware\CompressResponse::class,
+            //\Illuminate\Foundation\Http\Middleware\CompressResponse::class,
         ],
 
         'api' => [
