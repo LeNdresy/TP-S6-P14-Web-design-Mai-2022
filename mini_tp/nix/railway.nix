c37cbba58925d26fd01f968e567a7469940b223c
mandresyRandria <zilberthomandresy@gmail.com>
2023-05-08 20:09:57 +0200

    Delete railway.nix

--- mini_tp/nix/railway.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs ? import <nixpkgs> {} }:
-let
-  phpVersion = "8.0";
-in {
-  environment.systemPackages = with pkgs; [
-    php${phpVersion}.extensions.redis
-    php${phpVersion}.extensions.gd
-  ];
-}
